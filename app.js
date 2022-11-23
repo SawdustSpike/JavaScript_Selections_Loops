805c22abb3bbdd75d396ab4fe53cb4ed552ffdae
Mike Cowell <spike@spikeandbetty.com>
2022-11-22 22:17:29 -0500

    init commit

--- app.js
+++ app.js
@@ -2,6 +2,37 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(let i =0; i<=100; i+=2){
+    if(i%2==1){
+        console.log(i);
+    }
+    else{
+continue;
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for(let i = 0; i<= 100; i++){
+    if(i%5==0 && i%3==0){
+        console.log("FIZZBUZZ");
+    }
+    else if(i%5 == 0){
+        console.log("BUZZ");
+    }
+    else if(i%3 == 0){
+        console.log("FIZZ");
+    }
+}
+console.log("EXERCISE 3: \n===========\n");
+let x = 1;
+do{
+    console.log(x)
+    x +2;
+};
+while(x<100);
+
+
+console.log("EXERCISE 4: \n==============\n");
+
+
